d644d11a08b86ee5a682c716c48ed98b3a9be6fc
Chris Faulkner <chris.faulkner@digital.homeoffice.gov.uk>
2023-11-15 16:59:50 +0000

    Added to records

--- src/main/java/org/lucidant/java16/Book.java
+++ src/main/java/org/lucidant/java16/Book.java
@@ -1,11 +1,38 @@
 package org.lucidant.java16;
 
 import java.util.Locale;
+import java.util.regex.Pattern;
 
-public record Book(String title, Integer publicationYear, String author) {
+/**
+ * Records are immutable. All fields final. etc.
+ *
+ * @param title
+ * @param publicationYear
+ * @param author
+ * @param isbn
+ */
+public record Book(String title, Integer publicationYear, String author, Long isbn) {
+
+    /**
+     * Can still have constructors
+     * @param title
+     *   the title
+     * @param publicationYear
+     *   the year
+     * @param author
+     *   the author
+     */
+    Book(String title, Integer publicationYear, String author) {
+        this(title, publicationYear, author, null);
+    }
+
+    private static final String ISBN_FORMAT = "^\\d{13}$";
 
     public String formatAuthor() {
         return author.toUpperCase(Locale.ROOT);
     }
 
+    public boolean isValidIsbn() {
+        return Pattern.compile(ISBN_FORMAT).matcher(isbn.toString()).matches();
+    }
 }
